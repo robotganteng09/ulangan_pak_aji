94cd13b24338874bc937547eaa7bd8327c011194
ZEROOCPT <made.narendra.dhanadyatanurjaya@gmail.com>
2025-09-04 20:13:44 +0700

    Ini home page

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,6 +1,7 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:ulangan_pak_aji/pages/addTodoPage.dart';
+import 'package:ulangan_pak_aji/pages/editTodoPage.dart';
 import 'package:ulangan_pak_aji/pages/homePage.dart';
-
 import 'package:ulangan_pak_aji/pages/loginPage.dart';
 import 'package:ulangan_pak_aji/pages/navbar.dart';
 import 'package:ulangan_pak_aji/routes/route.dart';
@@ -9,5 +10,8 @@ class AppPage {
   static final pages = [
     GetPage(name: AppRoutes.Loginpage, page: () => Loginpage()),
     GetPage(name: AppRoutes.Homepage, page: () => Homepage()),
+    GetPage(name: AppRoutes.Addpage, page: () => AddPage()),
+    GetPage(name: AppRoutes.Editpage, page: () => EditTodoPage()),
+    GetPage(name: AppRoutes.Navbar, page: () => Navbar()),
   ];
 }
--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,4 +1,7 @@
 class AppRoutes {
   static const String Loginpage = "/LoginPage";
   static const String Homepage = "/HomePage";
+  static const String Addpage = "/AddPage";
+  static const String Editpage = "/EditPage";
+  static const String Navbar = "/Navbar";
 }
