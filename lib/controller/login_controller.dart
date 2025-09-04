c329356d528cd187a6e0453d012b01150e8579ce
robotganteng09 <arsyafaisyar@gmail.com>
2025-09-04 12:45:43 +0700

    Navbar [Arsya]

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -11,7 +11,7 @@ class LoginController extends GetxController {
 
   void login() {
     if (txtUser.text == user && txtPassword.text == password) {
-      Get.toNamed(AppRoutes.Homepage);
+      Get.toNamed(AppRoutes.Navbar);
     } else {
       Get.snackbar("Login Gagal", "password salaah");
     }
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -12,7 +12,7 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("halooo"),
+      body: Text("Hello Robot"),
     );
   }
 }
